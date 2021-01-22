fc1ae6d23044691431e65a0cfd891acfd7015d28
marwenkhorchani <marwenkhorchani7@gmail.com>
2021-01-22 11:00:18 +0100

    ath done

--- myNewApp/src/app/login/login.component.ts
+++ myNewApp/src/app/login/login.component.ts
@@ -25,16 +25,16 @@ export class LoginComponent implements OnInit {
       this.service
         .login(this.username, this.password, this.role)
         .subscribe((res: any) => {
-          if (res[0].username !== null && res[0].role === 'admin') {
+          if (res[0].username === this.username && res[0].password === this.password && res[0].role === 'admin') {
             localStorage.setItem('admin', JSON.stringify(res));
             localStorage['login_status'] = '1';
 
             this.router.navigate(['/']);
-          } else if (res[0].role === 'student') {
+          } else if (res[0].username === this.username && res[0].password === this.password && res[0].role === 'student') {
             localStorage['login_status'] = '1';
             localStorage.setItem('student', JSON.stringify(res));
             this.router.navigate(['/']);
-          } else if (res[0].role === 'teacher') {
+          } else if (res[0].username === this.username && res[0].password === this.password && res[0].role === 'teacher') {
             localStorage['login_status'] = '1';
             localStorage.setItem('teacher', JSON.stringify(res));
             this.router.navigate(['/']);
