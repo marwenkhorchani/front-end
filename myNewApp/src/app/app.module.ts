69f0827142ea26ccac59734dc73c96ae0bc524f6
oussama-medfai <oussama-medfai>
2021-01-22 13:08:04 +0100

    event

--- myNewApp/src/app/app.module.ts
+++ myNewApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './event/events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
